454bb96d49dcdb5f0efa4351987557cd4e9737de
Saiteja-323 <lalamsaiteja.22.cse@anits.edu.in>
2024-12-22 19:45:49 +0530

    inital Commit

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,55 @@
+var numButtons = document.querySelectorAll(".drum").length;
+for(var i=0;i<numButtons;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var buttonhtml = this.innerHTML;
+    makeSound(buttonhtml);
+    animate(buttonhtml);
+  });
+}
+document.addEventListener("keypress",function(event){
+  console.log(event.key);
+  makeSound(event.key);
+  animate(event.key);
+});
+function makeSound(key){
+  switch(key){
+    case 'w':
+      var tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case 'a':
+      var tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case 's':
+      var tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case 'd':
+      var tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case 'j':
+      var crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+    case 'k':
+      var kick = new Audio("./sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case 'l':
+      var snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+    default:
+      console.log(key);
+      break;
+  }
+}
+function animate(akey){
+  var newkey = document.querySelector("."+akey);
+  newkey.classList.add("pressed");
+  setTimeout(function(){
+    newkey.classList.remove("pressed");
+  },100);
+}
